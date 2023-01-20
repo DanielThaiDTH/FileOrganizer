6bd71749b2560e1c1332c447b1d76085b0eb6cc3
Daniel Thai <daniel.thai.can@gmail.com>
2023-01-20 01:22:44 -0500

    Added FileCollection class

---
+++ FileManagerDB/Entities/FileCollection.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerDB.Entities
+{
+    [Table("Collections")]
+    public class FileCollection
+    {
+        [PrimaryKey, AutoIncrement]
+        [Column("ID")]
+        public int ID { get; set; }
+
+        [Unique]
+        [Column("Name")]
+        public string Name { get; set; }
+
+
+    }
+}
